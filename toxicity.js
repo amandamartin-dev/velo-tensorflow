c42415a35f347a6ac57448a86280a8b7200e2a09
Amanda <97615019+amandamartin-dev@users.noreply.github.com>
2022-12-01 12:48:54 -0500

    Update toxicity.js
    move model set into classify

--- toxicity.js
+++ toxicity.js
@@ -4,16 +4,16 @@ import wixChatBackend from 'wix-chat-backend';
 let model;
 
 export async function predict(input, channelId) {
-    model = await toxicity.load();
     const predictions = await classify(input, channelId);
     return predictions
 
 }
 
 const classify = async (inputs, channelId) => {
+    model = await toxicity.load()
     const results = await model.classify(inputs);
-    const toxicity = results[6].results[0].match
-    if (toxicity === true) {
+    const toxicityResult = results[6].results[0].match
+    if (toxicityResult === true) {
         sendWarning(channelId)
     }
 
