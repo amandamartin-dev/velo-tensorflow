4665b186fe985c2e2951b515ce68cc873c652055
Amanda <97615019+amandamartin-dev@users.noreply.github.com>
2024-05-29 15:23:50 -0400

    Create wix-studio-version.js
    added web.js with updates to send happy path message and other modifications. This is still in progress

---
+++ wix-studio-version.js
@@ -0,0 +1,61 @@
+//This code is for use in a web.js file in Wix Studio
+
+import * as toxicity from '@tensorflow-models/toxicity';
+import { Permissions, webMethod } from "wix-web-module";
+import wixChatBackend from 'wix-chat-backend';
+require('@tensorflow/tfjs');
+
+let model;
+
+export const predict = webMethod(
+  Permissions.Anyone, 
+  async (message, channelId) => { 
+    try {
+       const predictions = await classify(message, channelId);
+    return predictions
+    } catch (error) {
+      console.error("Error"+ error)
+    }
+  
+  }
+);
+
+const classify = async (inputs, channelId) => {
+    model = await toxicity.load()
+    const results = await model.classify(inputs);
+    const toxicityResult = results[6].results[0].match
+    if (toxicityResult === true) {
+        sendWarning(channelId)
+    }else{
+      sendOk(channelId)
+    }
+
+};
+
+function sendWarning(channelId) {
+    wixChatBackend.sendMessage({
+            "messageText": "We don't do that here. Be nice and try again.",
+            "channelId": channelId,
+
+        })
+        .then(() => {
+            console.log("Chat message sent");
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+function sendOk(channelId) {
+    wixChatBackend.sendMessage({
+            "messageText": "Thank you for contacting us, someone will be with you soon",
+            "channelId": channelId,
+
+        })
+        .then(() => {
+            console.log("Chat message sent");
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
