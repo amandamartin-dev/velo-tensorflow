d14bf91b0a824e1af3dffa7112be5e3918c65d14
Amanda <97615019+amandamartin-dev@users.noreply.github.com>
2024-06-04 15:16:10 -0400

    Update events.js
    add chat directions

--- events.js
+++ events.js
@@ -2,5 +2,7 @@ import {predict} from "backend/toxicity"
 //for wix studio the import should look like this import {predict} from "backend/toxicity.web.js"
 
 export function wixChat_onMessage(event) {
-  predict(event.payload.text,event.channelId)
+    if (event.direction === "VisitorToBusiness") {
+        predict(event.payload.text, event.channelId)
+    }
 }
