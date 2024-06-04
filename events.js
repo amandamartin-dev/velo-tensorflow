175ffa392c3109d5cfa9ac235663f395250c6ae5
Amanda <97615019+amandamartin-dev@users.noreply.github.com>
2024-06-04 12:00:05 -0400

    Update events.js
    add a note about studio implementation

--- events.js
+++ events.js
@@ -1,4 +1,5 @@
 import {predict} from "backend/toxicity"
+//for wix studio the import should look like this import {predict} from "backend/toxicity.web.js"
 
 export function wixChat_onMessage(event) {
   predict(event.payload.text,event.channelId)
