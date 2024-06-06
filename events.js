7301bb558ae9b10e927946582bb8a7a36b95c684
Amanda <97615019+amandamartin-dev@users.noreply.github.com>
2024-06-06 13:04:35 -0400

    Update events.js
    remove comment

--- events.js
+++ events.js
@@ -1,5 +1,4 @@
-import {predict} from "backend/toxicity"
-//for wix studio the import should look like this import {predict} from "backend/toxicity.web.js"
+import {predict} from "backend/toxicity.web.js"
 
 export function wixChat_onMessage(event) {
     if (event.direction === "VisitorToBusiness") {
